ac3059081d87868d3f8fc01917f0de83fa4f3acb
Rodriguez-comienzo <yolanda.rodmor@educa.jcyl.es>
2025-02-28 10:21:45 +0100

    primer comit

---
+++ A023.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class A023{
+	public static void main(String[] args){
+		Scanner teclado=new Scanner(System.in);
+		int entero;
+	
+		System.out.println("introduce entero");
+		entero=teclado.nextInt();
+		suma=entero*(entero+1)/2;
+		System.out.println(suma ); 
+	}
+}
